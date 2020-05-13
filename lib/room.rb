5f9deb2ce4e7500e9a1aace39638dd4a8257a2bc
“Neeru <neeram85@gmail.com>
2020-05-13 09:24:11 -0600

    Complete iteration 1

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,26 @@
-class Rom
+class Room
+  attr_reader :category
 
   def initialize(category, length, width)
     @category = category
-    @length = 10
-    @width = width
+    @length = length
+    @width = width.to_i
   end
 
   def area
     @length * @width
   end
+
+  def is_painted?
+    @paint == true
+  end
+
+def paint
+  @paint = true
+end
+
+
+
+
+
 end
